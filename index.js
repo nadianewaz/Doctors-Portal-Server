f84f4c04b0df5afc53505e3a2ac5359db85a50cd
nadia-newaz <nadia.nitu55@gmail.com>
2023-07-27 13:25:59 +0600

    get appointment

--- index.js
+++ index.js
@@ -27,15 +27,23 @@ async function run(){
     const database = client.db('doctors_portal');
     const appointmentsCollection = database.collection('appointments');
 
+    app.get('/appointments',  async (req, res) =>{
+      const email = req.query.email; 
+      const query = { email: email } 
+      console.log(query); 
+      const cursor = appointmentsCollection.find(query); 
+      const appointments = await cursor.toArray();
+      res.json(appointments); 
+      
+    });
+
     
     app.post('/appointments',  async (req, res) =>{
         const appointment = req.body;
         const result = await appointmentsCollection.insertOne(appointment);
         console.log(result);
         res.json(result); 
-
-
-    });
+      });
 
 
 
