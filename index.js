1b21217657dcc22dd5410fb71b7fd0d1d1d31def
Nitu <aninditaahmed@proton.me>
2023-07-31 20:09:24 -0400

    Removing the old appointments get url

--- index.js
+++ index.js
@@ -26,19 +26,6 @@ async function run(){
     await client.connect();
     const database = client.db('doctors_portal');
     const appointmentsCollection = database.collection('appointments');
-    
-    // Appointments filer 
-    app.get('/appointments',  async (req, res) =>{
-      const email = req.query.email; 
-      const date = new Date(req.query.date).dayjs(date).format('DD/MM/YYYY'); 
-      // console.log(date);  
-      const query = { email: email, date: date } 
-      // console.log(query); 
-      const cursor = appointmentsCollection.find(query); 
-      const appointments = await cursor.toArray();
-      res.json(appointments); 
-      
-    });
 
     app.post('/appointments',  async (req, res) =>{
         const appointment = req.body;
